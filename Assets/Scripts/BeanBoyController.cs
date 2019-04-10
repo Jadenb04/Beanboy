28231337da42139685249123a6d9e1c34bdec4de
Code Gaucho <peter.wu13@gmail.com>
2019-04-10 10:56:28 -0700

    Beanboy 1st commit

---
+++ Assets/Scripts/BeanBoyController.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeanBoyController : MonoBehaviour
+{
+
+    [HideInInspector]
+    public bool facingRight = true;
+    [HideInInspector]
+    public bool jump = true;
+
+    public float moveForce = 365f;
+
+    public float maxSpeed = 5f;
+
+    public float jumpForce = 1000f;
+
+    public Transform groundCheck;
+
+    private bool grounded = false;
+    private Animator anim;
+    private Rigidbody2D rigidbody2D;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1
+            << LayerMask.NameToLayer("Ground"));
+
+
+        if (Input.GetButtonDown("Jump") && grounded == true)
+        {
+            jump = true;
+
+        }
+    }
+
+
+
+
+    private void FixedUpdate()
+    {
+        float horizontalAxis = Input.GetAxis("Horizontal");
+
+        anim.SetFloat("Speed", Mathf.Abs(horizontalAxis));
+
+        if (horizontalAxis * rigidbody2D.velocity.x < maxSpeed)
+        {
+            rigidbody2D.AddForce(Vector2.right * horizontalAxis * moveForce);
+
+        }
+
+        if (Mathf.Abs(rigidbody2D.velocity.x) > maxSpeed)
+        {
+            rigidbody2D.velocity = new Vector2(Mathf.Sign(rigidbody2D.velocity.x) * maxSpeed, rigidbody2D.velocity.y);
+
+        }
+
+        if (horizontalAxis > 0 && !facingRight == true)
+        {
+            Flip();
+
+        }
+
+        else if (horizontalAxis > 0 && facingRight == true)
+        {
+            Flip();
+        }
+
+        if (jump == true)
+        {
+            anim.SetTrigger("Jump");
+
+            rigidbody2D.AddForce(new Vector2(0f, jumpForce));
+
+            jump = false;
+        }
+
+
+
+    }   
+    
+    private void Flip()
+    {
+        facingRight = !facingRight;
+
+        Vector3 theScale = transform.localScale;
+
+        theScale.x *= -1;
+
+        transform.localScale = theScale;
+    }
+
+}
