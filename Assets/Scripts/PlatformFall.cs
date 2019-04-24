de3f82af881d5e7a4379b0769c8f2f6b5339fc96
Jaden Baker <jadenbaker824@gmail.com>
2019-04-24 08:52:25 -0700

    falling platforms

---
+++ Assets/Scripts/PlatformFall.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformFall : MonoBehaviour
+{
+    public float fallDelay = 1f;
+    private Rigidbody2D rigidbody2D;
+
+    /// <summary>
+   /// </summary>
+    
+    void Awake()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+
+        rigidbody2D.isKinematic = true;
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player") == true)
+        {
+            Invoke("Fall", fallDelay);
+        }
+    }
+
+    // Update is called once per frame
+    void Fall()
+    {
+        rigidbody2D.isKinematic = false;
+        
+    }
+}
